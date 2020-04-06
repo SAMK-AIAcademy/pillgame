e5d64b6421429ec1fd8b5fce6a2999afbb85605d
Joonas Nordlund <jonaz92@gmail.com>
2020-04-06 20:44:51 +0300

    Add Prettier as pre-commit hook, format code with Prettier

--- routes/menuStack.js
+++ routes/menuStack.js
@@ -1,5 +1,5 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
 
 // Import the screens to use in the stack.
 import Menu from '../screens/Menu';
@@ -7,40 +7,39 @@ import LevelMenu from '../screens/LevelMenu';
 import ResumeGame from '../screens/Resume';
 import Options from '../screens/Options';
 
-// Available transitions to screens from menu screen.  
+// Available transitions to screens from menu screen.
 const screens = {
-    menu:{
-        screen: Menu,
-        navigationOptions: {
-            title: 'Menu',
-            headerShown: false //this will hide the header
-        }
+  menu: {
+    screen: Menu,
+    navigationOptions: {
+      title: 'Menu',
+      headerShown: false, //this will hide the header
     },
-    resumeGame:{
-        screen: ResumeGame,
-        navigationOptions: {
-            title: 'Resume game',
-            headerShown: false       
-        }
+  },
+  resumeGame: {
+    screen: ResumeGame,
+    navigationOptions: {
+      title: 'Resume game',
+      headerShown: false,
     },
-    newGame:{
-        screen: LevelMenu,
-        navigationOptions: {
-            title: 'New game',
-            headerShown: false
+  },
+  newGame: {
+    screen: LevelMenu,
+    navigationOptions: {
+      title: 'New game',
+      headerShown: false,
+    },
+  },
+  options: {
+    screen: Options,
+    navigationOptions: {
+      title: 'Options',
+      headerShown: false,
+    },
+  },
+};
 
-        }
-    },
-    options:{
-        screen: Options,
-        navigationOptions: {
-            title: 'Options',
-            headerShown: false
-        }
-    }
-}
-
-// Variable to export. 
+// Variable to export.
 const MenuStack = createStackNavigator(screens);
 
 // Export the screens to use in the App.js.
--- screens/Menu.js
+++ screens/Menu.js
@@ -1,76 +1,80 @@
-
 import React from 'react';
-import { Dimensions } from "react-native";
-import { StyleSheet, View, Text } from 'react-native';
+import {Dimensions} from 'react-native';
+import {StyleSheet, View, Text} from 'react-native';
 
 // Get the full height of the used screen for later use.
 var height = Dimensions.get('window').height;
 
-export default function Menu({ navigation }){ 
-
+export default function Menu({navigation}) {
   // Handler for when "Resume game" is pressed in the menu screen.
   const resumeHandler = () => {
     navigation.navigate('resumeGame');
-  }
+  };
   // Handler for when "New game" is pressed in the menu screen.
   const newGameHandler = () => {
     navigation.navigate('newGame');
-  }
+  };
   // Handler for when "Options" is pressed in the menu screen.
   const optionsHandler = () => {
     navigation.navigate('options');
-  }
+  };
 
   // Dispaly the contents of the menu screen.
-  return(
-  <View style={styles.container}>
-    <Text style={styles.heading}>Pill game</Text>
-    <Text style={styles.resume} onPress={resumeHandler}>Resume game</Text>
-    <Text style={styles.newGame} onPress={newGameHandler}>New game</Text>
-    <Text style={styles.settings} onPress={optionsHandler}>Options</Text>
-  </View>
-  )
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Pill game</Text>
+      <Text style={styles.resume} onPress={resumeHandler}>
+        Resume game
+      </Text>
+      <Text style={styles.newGame} onPress={newGameHandler}>
+        New game
+      </Text>
+      <Text style={styles.settings} onPress={optionsHandler}>
+        Options
+      </Text>
+    </View>
+  );
 }
 
 // Define the styles used in the screen.
 const styles = StyleSheet.create({
-    container: {
-      height: height,
-      position: 'relative',
-      backgroundColor: '#fff',
-    },
-    resume: {
-      position: 'absolute',
-      top: height * 0.6,
-      alignSelf: "center",
-      margin: 0,
-      padding: 0,
-      backgroundColor: '#fff',
-      fontSize: 40,
-    },
-    newGame: {
-      position: 'absolute',
-      top: height * 0.7,
-      alignSelf: "center",
-      margin: 0,
-      padding: 0,
-      backgroundColor: '#fff',
-      fontSize: 40,
-    },
-    settings: {
+  container: {
+    height: height,
+    position: 'relative',
+    backgroundColor: '#fff',
+  },
+  resume: {
     position: 'absolute',
-    top: height * 0.8,
-    alignSelf: "center",
+    top: height * 0.6,
+    alignSelf: 'center',
     margin: 0,
     padding: 0,
     backgroundColor: '#fff',
     fontSize: 40,
-    },
-    heading: {
-      position: 'absolute',
-      top: height * 0.2,
-      transform: [{rotate:"-30deg"}],
-      alignSelf: "center",
-      fontSize: 50
-    }
+  },
+  newGame: {
+    position: 'absolute',
+    top: height * 0.7,
+    alignSelf: 'center',
+    margin: 0,
+    padding: 0,
+    backgroundColor: '#fff',
+    fontSize: 40,
+  },
+  settings: {
+    position: 'absolute',
+    top: height * 0.8,
+    alignSelf: 'center',
+    margin: 0,
+    padding: 0,
+    backgroundColor: '#fff',
+    fontSize: 40,
+  },
+  heading: {
+    position: 'absolute',
+    top: height * 0.2,
+    transform: [{rotate: '-30deg'}],
+    alignSelf: 'center',
+    fontSize: 50,
+  },
 });
